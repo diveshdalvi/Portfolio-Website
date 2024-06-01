f7ba567d47d8c2ace26435c869b1d401cb6eac36
diveshdalvi <dalvidivesh1@gmail.com>
2024-06-01 20:40:16 +0530

    introduction added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+
 const App = () => {
   return (
-    <div className=" bg-[#0C0C0C]  h-screen font-jetbrains text-white">
-      <div className=" flex justify-between bg-[#333333]  items-center">
-        <span className=" text-lg items-center justify-center ml-2">
+    <div className="bg-[#0C0C0C] h-screen font-jetbrains text-white">
+      <div className="flex justify-between bg-[#333333] items-center">
+        <span className="text-lg items-center justify-center ml-2">
           Portfolio:/Divesh Dalvi
         </span>
         <div className="flex">
-          <div className=" hover:bg-[#424242]  cursor-pointer  w-12 h-12 flex items-center justify-center">
+          <div className="hover:bg-[#424242] cursor-pointer w-12 h-12 flex items-center justify-center">
             <QuestionMarkIcon />
           </div>
-          <div className=" hover:bg-red-700 cursor-pointer  w-12 h-12 flex items-center justify-center">
+          <div className="hover:bg-red-700 cursor-pointer w-12 h-12 flex items-center justify-center">
             <CloseIcon />
           </div>
+        </div>
+      </div>
+      <div className="font-VT323">
+        <div className="flex flex-col border-green-600 m-4 mt-16">
+          <span className="text-4xl">Hello Everyone</span>
+          <div className="relative inline-block">
+            {/* <p className="text-6xl animate-typing whitespace-nowrap overflow-hidden border-r-4 w-full border-r-white inline-block">
+              I'm Divesh Dalvi
+            </p> */}
+            <p className="text-6xl ">
+              I'm Divesh Dalvi
+            </p>
+          </div>
+          <span className="text-2xl">Web Developer</span>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      VT323: ["VT323"],
-      jetbrains: ["JetBrains Mono"],
+    extend: {
+      fontFamily: {
+        VT323: ["VT323", "monospace"], // Add fallback font family
+        jetbrains: ["JetBrains Mono", "monospace"], // Add fallback font family
+      },
+      animation: {
+        typing: "typing 2s  infinite",
+        blink: "blink 1s step-end infinite",
+      },
+      keyframes: {
+        typing: {
+          from: { width: "0" },
+        },
+        blink: {
+          "50%": { "border-color": "transparent" },
+        },
+      },
     },
   },
   plugins: [],
